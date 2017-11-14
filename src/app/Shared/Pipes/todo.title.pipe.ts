a13c5a9f5f1450625710544adfe501c185853c1e
Santosh Varma Kosuri J <dev.santoshvarma@gmail.com>
2017-11-14 23:46:34 +0530

    Pipes Complete

--- src/app/Shared/Pipes/todo.title.pipe.ts
+++ src/app/Shared/Pipes/todo.title.pipe.ts
@@ -5,16 +5,25 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TitlePipe implements PipeTransform {
 
-  transform(value: string): any {
-    if(value && typeof value === 'string'){
-      let values = value.split(' ');
-      let valueClone  = value;
-      let firstCharacter = valueClone.substring(0,1).toUpperCase();
-      let rest = valueClone.substring(1, valueClone.length);
-      return firstCharacter + rest;
+  transform(input_value: string, trancate_after: number): string|null {
+    if (input_value && typeof input_value === 'string') {
+      const ValuesArray = input_value.split(' ');
+      let transformed_string = '';
+      // tslint:disable-next-line:forin
+      for (const value in ValuesArray) {
+        const firstCharacter = ValuesArray[value].substring(0, 1).toUpperCase();
+        const rest = ValuesArray[value].substring(1, ValuesArray[value].length);
+        transformed_string += ' ' + firstCharacter + rest;
+      }
+
+      if (transformed_string.length >= trancate_after) {
+        transformed_string = transformed_string.substring(0, trancate_after);
+        transformed_string += '...';
+      }
+
+      return transformed_string;
     } else {
       return null;
     }
   }
-
 }
